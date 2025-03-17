16587cc0981285d1fdfd6fdd602376da0751c47f
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2025-03-17 22:45:55 +0300

    Create db

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,6 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BlogContext>(options => {
+    var config = builder.Configuration;
+    var GetConnectionString = config.GetConnectionString("sql_connection");
+    options.UseSqlite(GetConnectionString);
+});
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
