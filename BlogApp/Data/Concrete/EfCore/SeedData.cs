ce9b4aec7072442b7cc2e63c3f921a69f973dc61
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2025-05-20 20:49:03 +0300

    Seed Data Method

--- BlogApp/Data/Concrete/EfCore/SeedData.cs
+++ BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -3,71 +3,72 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Data.Concrete.EfCore
 {
-    public static class  SeedData
+    public static class SeedData
     {
         public static void TestVerileriniDoldur(IApplicationBuilder app)
         {
-            var context = app.ApplicationServices.CreateAsyncScope().ServiceProvider.GetService<BlogContext>();
-
-            if(context != null)
+            using (var scope = app.ApplicationServices.CreateScope())
             {
-                if(context.Database.GetPendingMigrations().Any())
+                var context = scope.ServiceProvider.GetRequiredService<BlogContext>();
+
+                if (context.Database.GetPendingMigrations().Any())
                 {
                     context.Database.Migrate();
                 }
 
-                if(!context.Tags.Any())
+                if (!context.Tags.Any())
                 {
                     context.Tags.AddRange(
-                        new Tag { Text = "web programlama"},
-                        new Tag { Text = "backend"},
-                        new Tag { Text = "frontend"},
-                        new Tag { Text = "fullstack"},
-                        new Tag { Text = "php"}
+                        new Tag { Text = "web programlama" },
+                        new Tag { Text = "backend" },
+                        new Tag { Text = "frontend" },
+                        new Tag { Text = "fullstack" },
+                        new Tag { Text = "php" }
                     );
                     context.SaveChanges();
                 }
 
-                if(!context.Users.Any())
+                if (!context.Users.Any())
                 {
                     context.Users.AddRange(
-                        new User { UserName = "efedogan"},
-                        new User { UserName = "ilberortayli"}
+                        new User { UserName = "efedogan" },
+                        new User { UserName = "ilberortayli" }
                     );
                     context.SaveChanges();
-
                 }
 
-                if(!context.Posts.Any())
+                if (!context.Posts.Any())
                 {
                     context.Posts.AddRange(
-                        new Post {
+                        new Post
+                        {
                             Title = "Asp.net core",
                             Content = "Asp.net core dersleri",
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-10),
                             Tags = context.Tags.Take(3).ToList(),
-                            UserId  = 1
+                            UserId = 1
                         },
-                            new Post {
+                        new Post
+                        {
                             Title = "Php",
                             Content = "Php core dersleri",
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-20),
                             Tags = context.Tags.Take(2).ToList(),
-                            UserId  = 1
+                            UserId = 1
                         },
-                            new Post {
+                        new Post
+                        {
                             Title = "Django",
                             Content = "Django dersleri",
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-5),
                             Tags = context.Tags.Take(4).ToList(),
-                            UserId  = 2
+                            UserId = 2
                         }
                     );
                     context.SaveChanges();
-
                 }
             }
         }
--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<BlogContext>(options => {
     var config = builder.Configuration;
     var GetConnectionString = config.GetConnectionString("sql_connection");
@@ -12,6 +14,6 @@ var app = builder.Build();
 
 SeedData.TestVerileriniDoldur(app);
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
